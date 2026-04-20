e6f60d3e60efe0a1714db7740a4643758e4aab98
dashborr <danisarfas7@gmail.com>
2026-04-20 14:26:34 +0700

    menambahkan float nilai dan void print data

--- file.cpp
+++ file.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namesapace std;
+
+class Mahasiswa{
+    float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+} //batas class
